6d55b30e7273e3aff962f3fbdb045c2f8fa7cf78
Андрей Каликин <kinzo.x.beato@gmail.com>
2023-01-02 05:03:40 +0300

    wtf is context

--- repo/repo.go
+++ repo/repo.go
@@ -1,6 +1,7 @@
 package repo
 
 import (
+	"context"
 	"github.com/go-redis/redis/v8"
 	"log"
 	"math/rand"
@@ -10,9 +11,11 @@ import (
 
 type BoobaRepo struct {
 	client *redis.Client
+	ctx    context.Context
 }
 
 func (r BoobaRepo) InitCache(content []string) {
+	r.ctx = context.Background()
 	url := os.Getenv("REDIS_URL")
 	if url == "" {
 		log.Fatal("no Redis cache!")
@@ -26,7 +29,7 @@ func (r BoobaRepo) InitCache(content []string) {
 	r.client = redis.NewClient(opt)
 
 	for _, booba := range content {
-		_, err := r.client.RPush(r.client.Context(), "booba", booba).Result()
+		_, err := r.client.RPush(r.ctx, "booba", booba).Result()
 		if err != nil {
 			panic(err)
 		}
@@ -34,7 +37,7 @@ func (r BoobaRepo) InitCache(content []string) {
 }
 
 func (r BoobaRepo) GetBooba() (string, error) {
-	res, err := r.client.LRange(r.client.Context(), "booba", 0, -1).Result()
+	res, err := r.client.LRange(r.ctx, "booba", 0, -1).Result()
 	if err != nil {
 		return "", err
 	}
