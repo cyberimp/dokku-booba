9ae274d67a5834054af60dcf1c436c9e85f94838
Andrey Kalikin <kinzo.x.beato@gmail.com>
2024-02-23 15:06:17 +0300

    pls work

--- main.go
+++ main.go
@@ -69,8 +69,16 @@ func main() {
 		panic(err)
 	}
 
+	handler := func(p *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) {
+		return func(w http.ResponseWriter, r *http.Request) {
+			log.Println(r.URL)
+			r.Host = remote.Host
+			p.ServeHTTP(w, r)
+		}
+	}
+
 	proxy := httputil.NewSingleHostReverseProxy(remote)
-	http.Handle("/", proxy)
+	http.HandleFunc("/", handler(proxy))
 
 	http.HandleFunc(
 		"/hi", func(w http.ResponseWriter, r *http.Request) {
