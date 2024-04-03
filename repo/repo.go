6d2c367d8458045fee0bb8cf815797def38f9b0b
Andrey Kalikin <kinzo.x.beato@gmail.com>
2024-04-03 06:13:11 +0300

    updated some vulnerabilities, added redis

--- repo/repo.go
+++ repo/repo.go
@@ -1,7 +1,11 @@
 package repo
 
 import (
+	"context"
+	"github.com/redis/go-redis/v9"
+	"log"
 	"math/rand"
+	"os"
 	"sync"
 	"time"
 )
@@ -9,12 +13,33 @@ import (
 type BoobaRepo struct {
 	cache []int
 	mutex sync.RWMutex
+	rdb   *redis.Client
+	ctx   context.Context
+}
+
+func (r *BoobaRepo) redisInit(content []int) {
+	r.ctx = context.Background()
+	url := os.Getenv("REDIS_URL")
+	opts, err := redis.ParseURL(url)
+	if err != nil {
+		log.Fatal("error getting data for redis connection:", err)
+	}
+	r.rdb = redis.NewClient(opts)
+
+	anyContent := make([]any, 0, len(content))
+	for _, num := range content {
+		anyContent = append(anyContent, num)
+	}
+	r.rdb.SAdd(r.ctx, "booba_new", anyContent...)
+	log.Print("Pushed into Redis!")
 }
 
 func (r *BoobaRepo) InitCache(content []int) {
 	r.mutex.Lock()
 	defer r.mutex.Unlock()
 	r.cache = content
+
+	go r.redisInit(content)
 }
 
 func (r *BoobaRepo) GetBooba() (int, error) {
