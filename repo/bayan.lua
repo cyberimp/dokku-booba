df2ad06d0f8e965f78ca6cf552ab52251b19f480
Andrey Kalikin <kinzo.x.beato@gmail.com>
2024-04-05 07:51:29 +0300

    now trims list right

--- repo/bayan.lua
+++ repo/bayan.lua
@@ -6,12 +6,12 @@ local trim = tonumber(ARGV[2])
 redis.call('SADD', set, newID)
 local length = tonumber(redis.call('LPUSH', list, newID))
 if length > trim then
-    local diff = length-trim+1
+    local diff = length-trim
     local result = redis.call('LRANGE', list, -diff, -1)
     for i in result do
         redis.call('SREM', set, i)
     end
-    redis.call('LTRIM', list, 0, length-1)
+    redis.call('LTRIM', list, 0, trim-1)
 end
 
 return 1
