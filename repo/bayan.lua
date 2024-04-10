e0ba3c73cbab743a6d35b7243406ed1090926aa3
Andrey Kalikin <kinzo.x.beato@gmail.com>
2024-04-10 20:09:38 +0300

    fixed for

--- repo/bayan.lua
+++ repo/bayan.lua
@@ -11,8 +11,8 @@ local length = tonumber(redis.call('LPUSH', list, newID))
 if length > trim then
     local diff = length-trim
     local result = redis.call('LRANGE', list, -diff, -1)
-    for i in result do
-        redis.call('SREM', set, i)
+    for k,v in pairs(result) do
+        redis.call('SREM', set, v)
     end
     redis.call('LTRIM', list, 0, trim-1)
 end
